954374174013f08345be1efaa43cc4785d5c2b34
XxXxfredxXxX <fridtjof.reichmann@gmail.com>
2021-11-30 12:42:48 +0100

    Car realised

--- src/GameObjects/Car.java
+++ src/GameObjects/Car.java
@@ -2,31 +2,31 @@ package GameObjects;
 
 /**
  *
- * @author 
+ * @author fridtjof
  */
-public class Car {
+public class Car extends Look{
     private final int speed = 0;
     private int direction;
     public final int stepsize = 0;
 
     public int getSpeed() {
-        return speed;
+        return this.speed;
     }
 
     public int getDirection() {
-        return direction;
+        return this.direction;
     }
 
     public int getStepsize() {
-        return stepsize;
+        return this.stepsize;
     }
     
     public void setDirection(int direction){
-        
+        this.direction = direction;
     }
     
     public void colideFrog(Frog frog){
-        
+        frog.die();
     }
     
 }
