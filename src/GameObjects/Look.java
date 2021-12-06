1c1a975608b292989b4b917f627c505e7153be19
TM <TM@MacBookPro16.fritz.box>
2021-12-06 23:10:36 +0100

    look.java done

--- src/GameObjects/Look.java
+++ src/GameObjects/Look.java
@@ -1,19 +1,30 @@
 package GameObjects;
 
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+
 /**
  *
- * @author timo,fridtjof
+ * @author timo, fridtjof
  */
 public class Look {
     private String picturePath;
     private int[] pictureDimension;
 
-    public void setPicturePath(String picturePath) {
+    public void setPicturePath(String picturePath) throws IOException {
+        BufferedImage bimg = ImageIO.read(new File(picturePath));
+        this.setPictureDimension(bimg.getWidth(), bimg.getHeight());
         this.picturePath = picturePath;
     }
 
     private void setPictureDimension(int x, int y) {
-        this.pictureDimension = pictureDimension;
+        this.pictureDimension = new int[] {x,y};
+    }
+    
+    public int[] getPictureDimension(){
+        return this.pictureDimension;
     }
     
 }
